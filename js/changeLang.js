5a5334eb3c69484d57d0f1b4d02c29b9f8bf1398
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-02-27 21:39:39 +0100

    update changeLang

--- js/changeLang.js
+++ js/changeLang.js
@@ -1,90 +1,75 @@
-// Toggle language
 const buttonBritish = document.querySelector("#en-lang");
 const buttonSpanish = document.querySelector("#es-lang");
-const homeText = document.querySelector("#homeText");
-const aboutNavText = document.querySelector("#aboutNavText");
-const imText = document.querySelector("#imText");
-const curriculumText = document.querySelector("#curriculumText");
-const aboutText = document.querySelector("#aboutText");
-const aboutMeText = document.querySelector("#aboutMeText");
-const professionText = document.querySelector("#professionText");
-const descriptionText = document.querySelector("#descriptionText");
-const nameTitle = document.querySelector("#nameTitle");
-const birthdateTitle = document.querySelector("#birthdateTitle");
-const phoneTitle = document.querySelector("#phoneTitle");
-const addressTitle = document.querySelector("#addressTitle");
-const experienceTitle = document.querySelector("#experienceTitle");
-const birthdateText = document.querySelector("#birthdateText");
-const addressText = document.querySelector("#addressText");
-const experienceText = document.querySelector("#experienceText");
-const mySkillsText = document.querySelector("#mySkillsText");
-const galleryText = document.querySelector("#galleryText");
-const myPortfolioText = document.querySelector("#myPortfolioText");
-const boomTitle = document.querySelector("#boomTitle");
-const instaPetsTitle = document.querySelector("#instaPetsTitle");
-const soonTitle = document.querySelector("#soonTitle");
 
 buttonBritish.addEventListener("click", function () {
-  changeLanguage("eng");
+  translateTo("en");
 });
 
 buttonSpanish.addEventListener("click", function () {
-  changeLanguage("es");
+  translateTo("es");
 });
 
-function changeLanguage(lang) {
-  location.hash = lang;
-  location.reload();
+function translateTo(newLang) {
+  var elements = document.querySelectorAll("[data-translate]");
+
+  Array.from(elements).map(function (element) {
+    var key = element.getAttribute("data-translate");
+    element.textContent = translations[newLang][key];
+  });
+
+  document.documentElement.lang = newLang;
+  lang = newLang;
 }
 
-var language = {
+var translations = {
   es: {
-    home: "Inicio",
-    about: "Sobre",
-    im: "Soy",
-    curriculum: "Descargar CV",
-    aboutMe: "Sobre mí",
-    profession: "Desarrolladora FullStack",
-    description:
+    homeText: "Inicio",
+    aboutNavText: "Sobre",
+    imText: "Soy",
+    curriculumText: "Descargar CV",
+    aboutText: "Sobre",
+    aboutMeText: "Sobre mí",
+    professionText: "Desarrolladora FullStack",
+    descriptionText:
       "Soy una persona adaptable y confiable, con excelentes habilidades y un enfoque responsable hacia mi trabajo. Tengo una mentalidad flexible que me permite manejar eficazmente diversas situaciones y desafíos. Además, disfruto trabajando en equipo y estoy comprometida con el éxito del grupo.",
     nameTitle: "Nombre:",
     birthdateTitle: "Fecha:",
     birthdateText: "14/10/1993",
-    phoneTitle: "Teléfono:",
     addressTitle: "Dirección:",
     addressText: "San Sebastián, Gipuzkoa",
     experienceTitle: "Experiencia: ",
     experienceText: "8 Meses",
-    mySkills: "Mis Skills",
-    gallery: "Galería",
-    myPortfolio: "Mi portfolio",
+    phoneTitle: "Teléfono:",
+    mySkillsText: "Mis Skills",
+    galleryText: "Galería",
+    myPortfolioText: "Mi portfolio",
     boomTitle: "Boom! - Juego de preguntas",
     instaPetsTitle: "Instapets - API (en producción)",
     soonTitle: "Pronto...",
   },
+  en: {
+    homeText: "Home",
+    aboutNavText: "About",
+    imText: "I'm",
+    curriculumText: "Download CV",
+    aboutText: "About",
+    aboutMeText: "About me",
+    professionText: "FullStack Developer",
+    descriptionText:
+      "I am an adaptable and reliable person with excellent skills and a responsible approach to my work. I have a flexible mindset that enables me to effectively handle diverse situations and challenges. Additionally, I enjoy working in a team and I am committed to the group's success.",
+    nameTitle: "Name:",
+    birthdateTitle: "Birthday:",
+    birthdateText: "14/10/1993",
+    addressTitle: "Address:",
+    addressText: "St Sebastian, Guipuzcoa",
+    experienceTitle: "Experience: ",
+    experienceText: "8 Months",
+    phoneTitle: "Phone:",
+    mySkillsText: "My Skills",
+    galleryText: "Gallery",
+    myPortfolioText: "My portfolio",
+    boomTitle: "Boom! - Quiz game",
+    instaPetsTitle: "Instapets - API (producing)",
+    soonTitle: "Soon...",
+  },
 };
-
-if (window.location.hash == "#es") {
-  homeText.textContent = language.es.home;
-  aboutNavText.textContent = language.es.aboutMe;
-  imText.textContent = language.es.im;
-  curriculumText.textContent = language.es.curriculum;
-  aboutText.textContent = language.es.about;
-  aboutMeText.textContent = language.es.aboutMe;
-  professionText.textContent = language.es.profession;
-  descriptionText.textContent = language.es.description;
-  nameTitle.textContent = language.es.nameTitle;
-  birthdateTitle.textContent = language.es.birthdateTitle;
-  birthdateText.textContent = language.es.birthdateText;
-  phoneTitle.textContent = language.es.phoneTitle;
-  addressTitle.textContent = language.es.addressTitle;
-  addressText.textContent = language.es.addressText;
-  experienceTitle.textContent = language.es.experienceTitle;
-  experienceText.textContent = language.es.experienceText;
-  mySkillsText.textContent = language.es.mySkills;
-  galleryText.textContent = language.es.gallery;
-  myPortfolioText.textContent = language.es.myPortfolio;
-  boomTitle.textContent = language.es.boomTitle;
-  instaPetsTitle.textContent = language.es.instaPetsTitle;
-  soonTitle.textContent = language.es.soonTitle;
-}
