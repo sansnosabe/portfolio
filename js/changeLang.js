109cffb8524b76a09212328702b8e17c02cefe99
Sandra Erentxun <sandraerentxun93@gmail.com>
2023-03-15 21:51:39 +0100

    change html semantic

--- js/changeLang.js
+++ js/changeLang.js
@@ -1,75 +1,77 @@
+"use strict";
+
 const buttonBritish = document.querySelector("#en-lang");
 const buttonSpanish = document.querySelector("#es-lang");
 
 buttonBritish.addEventListener("click", function () {
-  translateTo("en");
+	translateTo("en");
 });
 
 buttonSpanish.addEventListener("click", function () {
-  translateTo("es");
+	translateTo("es");
 });
 
 function translateTo(newLang) {
-  var elements = document.querySelectorAll("[data-translate]");
+	var elements = document.querySelectorAll("[data-translate]");
 
-  Array.from(elements).map(function (element) {
-    var key = element.getAttribute("data-translate");
-    element.textContent = translations[newLang][key];
-  });
+	Array.from(elements).map(function (element) {
+		var key = element.getAttribute("data-translate");
+		element.textContent = translations[newLang][key];
+	});
 
-  document.documentElement.lang = newLang;
-  lang = newLang;
+	document.documentElement.lang = newLang;
+	lang = newLang;
 }
 
 var translations = {
-  es: {
-    homeText: "Inicio",
-    aboutNavText: "Sobre",
-    imText: "Soy",
-    curriculumText: "Descargar CV",
-    aboutText: "Sobre",
-    aboutMeText: "Sobre mí",
-    professionText: "Desarrolladora FullStack",
-    descriptionText:
-      "Soy una persona adaptable y confiable, con excelentes habilidades y un enfoque responsable hacia mi trabajo. Tengo una mentalidad flexible que me permite manejar eficazmente diversas situaciones y desafíos. Además, disfruto trabajando en equipo y estoy comprometida con el éxito del grupo.",
-    nameTitle: "Nombre:",
-    birthdateTitle: "Fecha:",
-    birthdateText: "14/10/1993",
-    addressTitle: "Dirección:",
-    addressText: "San Sebastián, Gipuzkoa",
-    experienceTitle: "Experiencia: ",
-    experienceText: "8 Meses",
-    phoneTitle: "Teléfono:",
-    mySkillsText: "Mis Skills",
-    galleryText: "Galería",
-    myPortfolioText: "Mi portfolio",
-    boomTitle: "Boom! - Juego de preguntas",
-    instaPetsTitle: "Instapets - API",
-    soonTitle: "Pronto...",
-  },
-  en: {
-    homeText: "Home",
-    aboutNavText: "About",
-    imText: "I'm",
-    curriculumText: "Download CV",
-    aboutText: "About",
-    aboutMeText: "About me",
-    professionText: "FullStack Developer",
-    descriptionText:
-      "I am an adaptable and reliable person with excellent skills and a responsible approach to my work. I have a flexible mindset that enables me to effectively handle diverse situations and challenges. Additionally, I enjoy working in a team and I am committed to the group's success.",
-    nameTitle: "Name:",
-    birthdateTitle: "Birthday:",
-    birthdateText: "14/10/1993",
-    addressTitle: "Address:",
-    addressText: "St Sebastian, Guipuzcoa",
-    experienceTitle: "Experience: ",
-    experienceText: "8 Months",
-    phoneTitle: "Phone:",
-    mySkillsText: "My Skills",
-    galleryText: "Gallery",
-    myPortfolioText: "My portfolio",
-    boomTitle: "Boom! - Quiz game",
-    instaPetsTitle: "Instapets - API",
-    soonTitle: "Soon...",
-  },
+	es: {
+		homeText: "Inicio",
+		aboutNavText: "Sobre",
+		imText: "Soy",
+		curriculumText: "Descargar CV",
+		aboutText: "Sobre",
+		aboutMeText: "Sobre mí",
+		professionText: "Desarrolladora FullStack",
+		descriptionText:
+			"Soy una persona adaptable y confiable, con excelentes habilidades y un enfoque responsable hacia mi trabajo. Tengo una mentalidad flexible que me permite manejar eficazmente diversas situaciones y desafíos. Además, disfruto trabajando en equipo y estoy comprometida con el éxito del grupo.",
+		nameTitle: "Nombre:",
+		birthdateTitle: "Fecha:",
+		birthdateText: "14/10/1993",
+		addressTitle: "Dirección:",
+		addressText: "San Sebastián, Gipuzkoa",
+		experienceTitle: "Experiencia: ",
+		experienceText: "8 Meses",
+		phoneTitle: "Teléfono:",
+		mySkillsText: "Mis Skills",
+		galleryText: "Galería",
+		myPortfolioText: "Mi portfolio",
+		boomTitle: "Boom! - Juego de preguntas",
+		instaPetsTitle: "Instapets - API",
+		soonTitle: "Pronto...",
+	},
+	en: {
+		homeText: "Home",
+		aboutNavText: "About",
+		imText: "I'm",
+		curriculumText: "Download CV",
+		aboutText: "About",
+		aboutMeText: "About me",
+		professionText: "FullStack Developer",
+		descriptionText:
+			"I am an adaptable and reliable person with excellent skills and a responsible approach to my work. I have a flexible mindset that enables me to effectively handle diverse situations and challenges. Additionally, I enjoy working in a team and I am committed to the group's success.",
+		nameTitle: "Name:",
+		birthdateTitle: "Birthday:",
+		birthdateText: "14/10/1993",
+		addressTitle: "Address:",
+		addressText: "St Sebastian, Guipuzcoa",
+		experienceTitle: "Experience: ",
+		experienceText: "8 Months",
+		phoneTitle: "Phone:",
+		mySkillsText: "My Skills",
+		galleryText: "Gallery",
+		myPortfolioText: "My portfolio",
+		boomTitle: "Boom! - Quiz game",
+		instaPetsTitle: "Instapets - API",
+		soonTitle: "Soon...",
+	},
 };
--- js/main.js
+++ js/main.js
@@ -1,92 +1,87 @@
-(function () {
-  "use strict";
+"use strict";
 
-  // Toggle Switch
-  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-  const currentTheme = localStorage.getItem("theme");
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem("theme");
 
-  if (currentTheme) {
-    document.documentElement.setAttribute("data-theme", currentTheme);
+if (currentTheme) {
+	document.documentElement.setAttribute("data-theme", currentTheme);
 
-    if (currentTheme === "dark") {
-      toggleSwitch.checked = true;
-    } else {
-      toggleSwitch.checked = false;
-    }
-  }
+	if (currentTheme === "dark") {
+		toggleSwitch.checked = true;
+	} else {
+		toggleSwitch.checked = false;
+	}
+}
 
-  function switchTheme(e) {
-    if (e.target.checked) {
-      document.documentElement.setAttribute("data-theme", "dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.setAttribute("data-theme", "light");
-      localStorage.setItem("theme", "light");
-    }
-  }
+function switchTheme(e) {
+	if (e.target.checked) {
+		document.documentElement.setAttribute("data-theme", "dark");
+		localStorage.setItem("theme", "dark");
+	} else {
+		document.documentElement.setAttribute("data-theme", "light");
+		localStorage.setItem("theme", "light");
+	}
+}
 
-  toggleSwitch.addEventListener("change", switchTheme, false);
+toggleSwitch.addEventListener("change", switchTheme, false);
 
-  // Navbar on scrolling
-  // window.addEventListener("scroll", function () {
-  //   if (window.scrollY > 200) {
-  //     document.querySelector(".navbar").style.display = "flex";
-  //   } else {
-  //     document.querySelector(".navbar").style.display = "none";
-  //   }
-  // });
+// Navbar on scrolling
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > 200) {
+//     document.querySelector(".navbar").style.display = "flex";
+//   } else {
+//     document.querySelector(".navbar").style.display = "none";
+//   }
+// });
 
-  // Back to top button
-  const backToTop = document.querySelector(".back-to-top");
+const backToTop = document.querySelector(".back-to-top");
 
-  window.addEventListener("scroll", function () {
-    if (window.pageYOffset > 200) {
-      backToTop.style.display = "block";
-    } else {
-      backToTop.style.display = "none";
-    }
-  });
+window.addEventListener("scroll", function () {
+	if (window.pageYOffset > 200) {
+		backToTop.style.display = "block";
+	} else {
+		backToTop.style.display = "none";
+	}
+});
 
-  backToTop.addEventListener("click", function (e) {
-    e.preventDefault();
-    const scrollDuration = 700;
-    const scrollStep = -window.scrollY / (scrollDuration / 15);
-    const scrollInterval = setInterval(function () {
-      if (window.scrollY !== 0) {
-        window.scrollBy(0, scrollStep);
-      } else {
-        clearInterval(scrollInterval);
-      }
-    }, 15);
-  });
+backToTop.addEventListener("click", function (e) {
+	e.preventDefault();
+	const scrollDuration = 700;
+	const scrollStep = -window.scrollY / (scrollDuration / 15);
+	const scrollInterval = setInterval(function () {
+		if (window.scrollY !== 0) {
+			window.scrollBy(0, scrollStep);
+		} else {
+			clearInterval(scrollInterval);
+		}
+	}, 15);
+});
 
-  // Smooth scrolling on the navbar links (scroll del menu)
-  const navbarLinks = document.querySelectorAll(".navbar-nav a");
-  navbarLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      if (link.hash !== "") {
-        event.preventDefault();
+const navbarLinks = document.querySelectorAll(".navbar-nav a");
+navbarLinks.forEach((link) => {
+	link.addEventListener("click", (event) => {
+		if (link.hash !== "") {
+			event.preventDefault();
 
-        const target = document.querySelector(link.hash);
-        if (target) {
-          target.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-          });
-        }
-      }
-    });
-  });
+			const target = document.querySelector(link.hash);
+			if (target) {
+				target.scrollIntoView({
+					behavior: "smooth",
+					block: "start",
+				});
+			}
+		}
+	});
+});
 
-  const typedText = document.querySelector(".typed-text");
-  const typedTextOutput = document.querySelector(".typed-text-output");
-  if (typedText && typedTextOutput) {
-    const typed = new Typed(typedTextOutput, {
-      strings: typedText.textContent.split(", "),
-      typeSpeed: 100,
-      backSpeed: 20,
-      smartBackspace: false,
-      loop: false,
-    });
-  }
-})();
+const typedText = document.querySelector(".typed-text");
+const typedTextOutput = document.querySelector(".typed-text-output");
+if (typedText && typedTextOutput) {
+	const typed = new Typed(typedTextOutput, {
+		strings: typedText.textContent.split(", "),
+		typeSpeed: 100,
+		backSpeed: 20,
+		smartBackspace: false,
+		loop: false,
+	});
+}
